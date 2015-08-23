7029557c8e00dfbf13b4a124b978a0408fc8216b
Ketan Shukla <ketankshukla@gmail.com>
2015-08-23 00:32:40 -0700

    Assignment 25 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   #The following line is an implied hash
   #It could also be re-written as root({to: 'welcome#index'})
   root to: 'welcome#index'
