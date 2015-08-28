6512de93bea13527ba54896b17f3657ef5b9bfb8
Ketan Shukla <ketankshukla@gmail.com>
2015-08-28 16:47:53 -0700

    Checkpoint 35 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,13 +4,19 @@ include RandomData
 RSpec.describe Post, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
 
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   it { should belong_to(:topic) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
 
   context "attributes" do
 
