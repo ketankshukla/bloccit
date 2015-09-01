797390b9dcf49522be3583192d64b97f6319f9a2
Ketan Shukla <ketankshukla@gmail.com>
2015-09-01 00:23:57 -0700

    Checkpoint 40 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,10 +39,19 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-    email: 'ketandev@gmail.com', # replace this with your personal email
-    password: 'blocprogram'
+
+admin = User.create!(
+    name:     'Admin User',
+    email:    'admin@example.com',
+    password: 'helloworld',
+    role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+    name:     'Member User',
+    email:    'member@example.com',
+    password: 'helloworld'
 )
 
 puts "Seed finished"
