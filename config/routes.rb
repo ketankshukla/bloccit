e2b923923851381d47c4f8aedfaffdc32536a5ae
Ketan Shukla <ketankshukla@gmail.com>
2015-09-02 22:52:47 -0700

    Checkpoint 41 completed

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,11 @@ Rails.application.routes.draw do
   #resources :topics
   #resources :posts
   resources :topics do
-    # #34
     resources :posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:new, :create]
