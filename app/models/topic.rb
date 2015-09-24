74bd1f2de2777faab00a0916b0d9201d3084c4f7
Ketan Shukla <ketankshukla@gmail.com>
2015-09-24 11:30:55 -0700

    Checkpoint 50 private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,9 @@
 class Topic < ActiveRecord::Base
-  has_many :posts
+
   has_many :posts, dependent: :destroy
   has_many :labelings, as: :labelable
-  # #14
   has_many :labels, through: :labelings
 
-  default_scope { order('created_at DESC') }
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
 end
