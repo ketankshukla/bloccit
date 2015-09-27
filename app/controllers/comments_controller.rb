d4364dbba15b57f208659bf6837daecf9644e38f
Ketan Shukla <ketankshukla@gmail.com>
2015-09-26 17:09:47 -0700

    Checkpoint 51 Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,38 +4,44 @@ class CommentsController < ApplicationController
   before_action :authorize_user, only: [:destroy]
 
   def create
-    # #11
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.new(comment_params)
-    comment.user = current_user
 
-    if comment.save
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    @new_comment = Comment.new
+
+    if @comment.save
       flash[:notice] = "Comment saved successfully."
-      # #12
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment failed to save."
-      # #13
-      redirect_to [@post.topic, @post]
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
+    @comment = @post.comments.find(params[:id])
 
-    if comment.destroy
+    if @comment.destroy
       flash[:notice] = "Comment was deleted."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   private
 
-  # #14
   def comment_params
     params.require(:comment).permit(:body)
   end
