4e749087d35fa8b39d2aafd3ff2949fba640b93a
Ketan Shukla <ketankshukla@gmail.com>
2015-10-14 13:40:33 -0700

    Schema file

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150927032343) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
@@ -79,24 +71,6 @@ ActiveRecord::Schema.define(version: 20150927032343) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "ratings", force: :cascade do |t|
-    t.integer  "severity"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "ratings", ["rateable_id"], name: "index_ratings_on_rateable_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
